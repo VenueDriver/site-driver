0e0455d61892024c4d475bb4cf0b6bef9ac68b4e
Mariana Esteban <mariana@thebytery.com>
2017-09-05 17:42:28 -0300

    table layout update

--- public/js/main.js
+++ public/js/main.js
@@ -25,6 +25,43 @@ $(document).ready(function() {
 		}
 	}
 
+	$(".search-icon").on('click',function() {
+	  var icon = $(this),
+	      input = icon.parent().find("#search-field"),
+	      submit = icon.parent().find(".submit"),
+	      is_submit_clicked = false;
+	  
+	  // Animate the input field
+	  input.animate({
+	    "width": "165px",
+	    "padding": "10px",
+	    "opacity": 1
+	  }, 300, function() {
+	    input.focus();
+	  });
+	  
+	  submit.mousedown(function() {
+	    is_submit_clicked = true;
+	  });
+	  
+	  // Now, we need to hide the icon too
+	  icon.fadeOut(300);
+	  
+	  // Looks great, but what about hiding the input when it loses focus and doesnt contain any value? Lets do that too
+	  input.blur(function() {
+	    if(!input.val() && !is_submit_clicked) {
+	      input.animate({
+	        "width": "0",
+	        "padding": "0",
+	        "opacity": 0
+	      }, 200);
+	      
+	      // Get the icon back
+	      icon.fadeIn(200);
+	    };
+	  }); 
+	});
+
 	//ACORDEON
 
 	// $('#accordion div.panel').hide();
@@ -32,42 +69,7 @@ $(document).ready(function() {
 	//     $(this).next('div.panel').slideToggle('fast').siblings('div.panel:visible').slideUp('fast');
 	//     $(this).toggleClass("openit").siblings().removeClass("openit");
 	// });
-$(".search-icon").on('click',function() {
-	var icon = $(this),
-			input = icon.parent().find("#search-field"),
-			submit = icon.parent().find(".submit"),
-			is_submit_clicked = false;
-	
-	// Animate the input field
-	input.animate({
-		"width": "165px",
-		"padding": "10px",
-		"opacity": 1
-	}, 300, function() {
-		input.focus();
-	});
-	
-	submit.mousedown(function() {
-		is_submit_clicked = true;
-	});
-	
-	// Now, we need to hide the icon too
-	icon.fadeOut(300);
-	
-	// Looks great, but what about hiding the input when it loses focus and doesnt contain any value? Lets do that too
-	input.blur(function() {
-		if(!input.val() && !is_submit_clicked) {
-			input.animate({
-				"width": "0",
-				"padding": "0",
-				"opacity": 0
-			}, 200);
-			
-			// Get the icon back
-			icon.fadeIn(200);
-		};
-	});
-});
+
 
 
 });
