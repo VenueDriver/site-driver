c070a5684e3746dba48a313cad25aa17ddd157f9
Gaspar Errobidart <podmaster@hotmail.com>
2017-10-13 12:25:52 -0300

    incorporate into standard components
    Signed-off-by: Gaspar Errobidart <podmaster@hotmail.com>

--- front/components/molecule-browser/script.ts
+++ front/components/molecule-browser/script.ts
@@ -1,4 +1,6 @@
 import { Component , Input , OnInit , Output, EventEmitter, ComponentRef, ChangeDetectorRef} from '@angular/core';
+import { NgClass } from '@angular/common';
+import { DataService } from '../../services/data.service';
 
 
 @Component({
@@ -7,19 +9,52 @@ import { Component , Input , OnInit , Output, EventEmitter, ComponentRef, Change
   styles : [require('./styles.css')]
 })
 
-export class MoleculeBrowserComponent {
+export class MoleculeBrowserComponent implements OnInit {
 
   @Input() data ;
   @Output() valueChange = new EventEmitter();
 
-  constructor(private ref: ChangeDetectorRef){
+  @Input() userRole : number;
+  additional_classes : any = [];
+  isDeveloper : boolean = false;
+  errors : Array<any> = [];
+  editing : boolean = false;
+  ready : boolean = false;
 
+  constructor(private ref: ChangeDetectorRef, private dataService : DataService){
+
+  }
+
+  parseAdditionalClasses(){
+    if(this.data._options){
+      if(this.data._options._additional_css_classes) this.additional_classes = this.data._options._additional_css_classes.split(',');
+    }
+  }
+
+  ngOnInit(){
+    this.parseAdditionalClasses();
+    this.validate(this.data._value);
+    this.dataService.userRole().then((data)=>{
+      this.isDeveloper = (<any>data).role > 9000;
+      this.ready = true;
+    })
+  }
+
+  ngOnChanges(){
+    this.parseAdditionalClasses();
   }
 
   emitValue(hierarchyTree){
     this.valueChange.emit(hierarchyTree);
     this.data._value = hierarchyTree;
     this.ref.detectChanges();
+  }
+
+  validate(value){
+    return new Promise((resolve,reject)=>{
+      this.errors = [];
+      resolve(true);
+    })
   }
 
 }
