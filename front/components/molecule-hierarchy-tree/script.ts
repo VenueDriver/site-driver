5fdac2064f95eb9f9197c3cf3ea130b57ce5bf28
Gaspar Errobidart <podmaster@hotmail.com>
2017-10-17 15:39:50 -0300

    PD-785
    Signed-off-by: Gaspar Errobidart <podmaster@hotmail.com>

--- front/components/molecule-hierarchy-tree/script.ts
+++ front/components/molecule-hierarchy-tree/script.ts
@@ -28,6 +28,141 @@ export class MoleculeHierarchyTreeComponent implements OnInit {
 
   }
 
+  ngOnInit(){
+    this.moleculeService.getMoleculeList({
+      type : ["instance","generator"]
+    }).then((cache)=>{
+      this.ready = true;
+      this._og_list = cache.data;
+      if(this.root){
+        this._tree = this.root;
+        console.log("Tree provided",this._tree);
+      }else{
+        this._tree = this.buildNewTree();
+        console.log("Tree built",this._tree);
+      }
+      this.regenerateTree();
+    }).catch((err)=>{
+      console.log(err);
+    })
+  }
+
+  buildNewTree(branch : any = false){
+
+    const convertValueIntoBranch = (value,parent,index)=>{
+      if(value._branches) return value;
+
+      let _path_trace_to_root = parent._path_trace.slice();
+      _path_trace_to_root.push(index);
+
+      let value_branch_values = (Array.isArray(value._value)) ? [] : typeof value._value;
+
+      let currentBranch = {
+        _type : value._type,
+        _id : value._id,
+        _name : value._name,
+        _path_trace : _path_trace_to_root,
+        _checked : true,
+        _branches : {
+          _all : true,
+          _include : [],
+          _exclude : [],
+          _value : value_branch_values
+        }
+      };
+
+      if(value._ngClass !== "MoleculeGenerator" && Array.isArray(value._value)){
+        value_branch_values = value._value.map( (el,i) => convertValueIntoBranch(el,currentBranch,i));
+      }
+      currentBranch._branches._value = value_branch_values;
+
+      return currentBranch;
+    }
+
+    if(!branch){
+      branch = {
+        _type : "root",
+        _id : "",
+        _name : "root",
+        _path_trace : [],
+        _checked : true,
+        _branches : {
+          _all : true,
+          _include : [],
+          _exclude : [],
+          _value : []
+        }
+      }
+    }
+
+    //FETCH ALL CHILDS FOR THIS BRANCH
+    let childs = this._og_list.filter((el)=>{
+      if(branch._id){
+        if(el._generator){
+          return el._generator._id == branch._id;
+        }
+      }else{
+        return el._type == "generator";
+      }
+      return false;
+    });
+
+    childs = childs.map( (el,i) => convertValueIntoBranch(el,branch,i) );
+
+    branch._branches._value = childs.map((branch)=>{
+      return this.buildNewTree(branch);
+    });
+
+    return branch;
+  }
+
+  regenerateTree(){
+
+  }
+
+
+  childChecked(branch){
+    if(this.checked){
+      let targetArray = this._tree._branches[ (this._tree._branches._all) ? "_exclude" : "_include"];
+      let targetIndex = null;
+      targetArray.forEach((el,i)=>{
+        if(el._id === branch._id){
+        targetIndex = i
+        }
+      });
+      if(targetIndex != null){
+        targetArray.splice(targetIndex,1);
+      }else{
+        targetArray.push(branch);
+      }
+      this.updateChilds();
+    }
+    this.branchChanged(this._tree);
+  }
+
+
+  updateChilds(){
+    if(this.isArrayValue){
+      this._tree._branches._value = (<HierarchyTreeInterface[]>this._tree._branches._value).map((childBranch)=>{
+        return this.isBranchChecked(childBranch,this._tree);
+      })
+    }
+  }
+
+
+  isBranchChecked(child,parent){
+    let includedALL = this._tree._branches._all;
+    let excludedBranch = this._tree._branches._exclude.find(el=> el._id === child._id);
+    let includedBranch = this._tree._branches._include.find(el=> el._id === child._id);
+    if(includedALL && excludedBranch){
+      child._checked = false;
+    }else if(!includedALL && !includedBranch){
+      child._checked = false;
+    }else{
+      child._checked = true;
+    }
+    return child;
+  }
 
   branchChanged( branch : HierarchyTreeInterface ){
     console.log("received branch",branch);
@@ -110,161 +245,7 @@ export class MoleculeHierarchyTreeComponent implements OnInit {
   }
 
 
-  childChecked(branch){
-    if(this.checked){
-      let targetArray = this._tree._branches[ (this._tree._branches._all) ? "_exclude" : "_include"];
-      let targetIndex = null;
-      targetArray.forEach((el,i)=>{
-        if(el._id === branch._id){
-        targetIndex = i
-        }
-      });
-      if(targetIndex != null){
-        targetArray.splice(targetIndex,1);
-      }else{
-        targetArray.push(branch);
-      }
-      this.updateChilds();
-    }
-    this.branchChanged(this._tree);
-  }
 
 
-  updateChilds(){
-    if(this.isArrayValue){
-      this._tree._branches._value = (<HierarchyTreeInterface[]>this._tree._branches._value).map((childBranch)=>{
-        return this.isBranchChecked(childBranch,this._tree);
-      })
-    }
-  }
-
-
-  isBranchChecked(child,parent){
-    let includedALL = this._tree._branches._all;
-    let excludedBranch = this._tree._branches._exclude.find(el=> el._id === child._id);
-    let includedBranch = this._tree._branches._include.find(el=> el._id === child._id);
-    if(includedALL && excludedBranch){
-      child._checked = false;
-    }else if(!includedALL && !includedBranch){
-      child._checked = false;
-    }else{
-      child._checked = true;
-    }
-    return child;
-  }
-
-
-  ngOnInit(){
-    this.moleculeService.getMoleculeList({
-      type : ["instance","generator"]
-    }).then((cache)=>{
-      this.ready = true;
-      this._og_list = cache.data;
-      this.rebuildTree();
-    }).catch((err)=>{
-      console.log(err);
-    })
-  }
-
-
-  buildTree(){
-    console.log("Root?",this.root);
-    if(this.root){
-      this._tree = this.root;
-    }else{
-      this._tree = {
-        _type : "root",
-        _id : "",
-        _name : "root",
-        _path_trace : [],
-        _checked : true,
-        _branches : {
-          _all : true,
-          _include : [],
-          _exclude : [],
-          _value : []
-        }
-      }
-    }
-    this._tree = this.buildTreeBranch(this._tree);
-    console.log("buildTree() : _tree",this._tree);
-  }
-
-
-  buildTreeBranch(branch : HierarchyTreeInterface) : HierarchyTreeInterface | null{
-    if(branch._branches._value.length > 0) return branch
-    let childs = [];
-
-    const convertValueIntoBranch = (value,parent,index)=>{
-      if(value._branches) return value;
-
-      let _path_trace_to_root = parent._path_trace.slice();
-      _path_trace_to_root.push(index);
-
-      let value_branch_values = (Array.isArray(value._value)) ? [] : typeof value._value;
-
-      let currentBranch = {
-        _type : value._type,
-        _id : value._id,
-        _name : value._name,
-        _path_trace : _path_trace_to_root,
-        _checked : true,
-        _branches : {
-          _all : true,
-          _include : [],
-          _exclude : [],
-          _value : value_branch_values
-        }
-      };
-
-
-
-      if(value._ngClass !== "MoleculeGenerator" && Array.isArray(value._value)){
-        value_branch_values = value._value.map( (el,i) => convertValueIntoBranch(el,currentBranch,i));
-      }
-
-      currentBranch._branches._value = value_branch_values;
-
-      return currentBranch;
-
-    }
-
-    if(branch._branches._all === false){
-      //USE ONLY THE CORRESPONDING CHILDS
-      childs = branch._branches._include;
-    }else{
-      //FETCH ALL CHILDS FOR THIS BRANCH
-      childs = this._og_list.filter((el)=>{
-        if(branch._id){
-          if(el._generator){
-            return el._generator._id == branch._id;
-          }
-        }else{
-          return el._type == "generator";
-        }
-        return false;
-      });
-
-      //EXCLUDE THE CORRESPONDING CHILDS
-      if(branch._branches._exclude.length > 0){
-        let excluded_ids = branch._branches._exclude.map((excluded)=> excluded._id);
-        childs.filter((el)=> excluded_ids.indexOf(el._id) < 0 );
-      }
-
-      childs = childs.map( (el,i) => convertValueIntoBranch(el,branch,i) );
-    }
-
-    branch._branches._value = childs.map((branch)=>{
-      return this.buildTreeBranch(branch);
-    });
-
-    return branch;
-  }
-
-
-  rebuildTree(){
-    this._tree = null;
-    this.buildTree();
-  }
 
 }
