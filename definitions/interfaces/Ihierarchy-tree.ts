52af09fb5b1b9dbf646dbe1f5d4b5b137a2e4bc8
GasparErrobidart <podmaster@hotmail.com>
2017-10-19 23:10:24 -0300

    add selected property

--- definitions/interfaces/Ihierarchy-tree.ts
+++ definitions/interfaces/Ihierarchy-tree.ts
@@ -6,5 +6,6 @@ export interface HierarchyTreeInterface{
   _name : string,
   _path_trace : Array<number>,
   _branches : HierarchyTreeBranchInterface,
-  _checked : boolean
+  _checked : boolean,
+  _selected : boolean
 }
