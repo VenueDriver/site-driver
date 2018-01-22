e71c2040676f973e7ea4f946448ab3431f993268
Gaspar Errobidart <podmaster@hotmail.com>
2018-01-22 14:49:51 -0300

    migrate in steps
    Signed-off-by: Gaspar Errobidart <podmaster@hotmail.com>

--- tasks/s3-to-mongo-migrate.js
+++ tasks/s3-to-mongo-migrate.js
@@ -1,9 +1,11 @@
 const StorageRoutes = require('../config/storage');
-const storage = new StorageRoutes({ format : 'original' , type : ['cell','generator','instance'] });
+const cellsStorage = new StorageRoutes({ format : 'original' , type : ['cell'] });
+const generatorsStorage = new StorageRoutes({ format : 'original' , type : ['generator'] });
+const instancesStorage = new StorageRoutes({ format : 'original' , type : ['instance'] });
 const asyncLoop = require('../custom_modules/asyncloop');
 const uniqid = require('uniqid');
 
-storage.get().catch(err => console.error(err))
+cellsStorage.get().catch(err => console.error(err))
   .then((molecules)=>{
     let i = 0;
     let stopAt = molecules.length;
@@ -30,7 +32,69 @@ storage.get().catch(err => console.error(err))
           process.exit(1);
         }else{
           console.log("Exiting...");
-          process.exit(0);
+          generatorsStorage.get().catch(err => console.error(err))
+            .then((molecules)=>{
+              let i = 0;
+              let stopAt = molecules.length;
+              asyncLoop(
+                ()=> i >= stopAt,
+                (next,end)=>{
+                  let molecule = molecules[i];
+                  if(!molecule._id) molecule._id = uniqid();
+                  let query = {
+                    id : molecule._id,
+                    formattedData : molecule,
+                    name : molecule._name,
+                    type : molecule._type,
+                    format : 'original'
+                  };
+                  let storageInstance = new StorageRoutes(query);
+                  console.log("Saving.");
+                  storageInstance.save().catch(end).then(()=>{ i++ ; next(); });
+                },
+                (err)=>{
+                  if(err){
+                    console.error(err);
+                    console.log("Exiting with errors...");
+                    process.exit(1);
+                  }else{
+                    console.log("Exiting...");
+                    instancesStorage.get().catch(err => console.error(err))
+                      .then((molecules)=>{
+                        let i = 0;
+                        let stopAt = molecules.length;
+                        asyncLoop(
+                          ()=> i >= stopAt,
+                          (next,end)=>{
+                            let molecule = molecules[i];
+                            if(!molecule._id) molecule._id = uniqid();
+                            let query = {
+                              id : molecule._id,
+                              formattedData : molecule,
+                              name : molecule._name,
+                              type : molecule._type,
+                              format : 'original'
+                            };
+                            let storageInstance = new StorageRoutes(query);
+                            console.log("Saving.");
+                            storageInstance.save().catch(end).then(()=>{ i++ ; next(); });
+                          },
+                          (err)=>{
+                            if(err){
+                              console.error(err);
+                              console.log("Exiting with errors...");
+                              process.exit(1);
+                            }else{
+                              console.log("Exiting...");
+                              process.exit(0);
+                            }
+                          }
+                        );
+                      });
+                  }
+                }
+              );
+            });
         }
       }
     );
