d1a8b516b96b082aff24acbf121f3cefb5231f7a
GasparErrobidart <podmaster@hotmail.com>
2018-02-12 14:40:39 -0300

    Init test project

---
+++ simple-project/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Molecule = require("@molecule-driver/molecule");
+console.log(Molecule);
+process.exit(0);
+//# sourceMappingURL=index.js.map
---
+++ simple-project/index.ts
@@ -0,0 +1,5 @@
+import Molecule = require('@molecule-driver/molecule');
+
+console.log(Molecule);
+
+process.exit(0);
